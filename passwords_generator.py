947a2a966104a24f3c36bd26d4baadce3152fbe7
Pandasssss <shkod1998@mail.ru>
2020-09-12 01:32:27 +0300

    v.1.5 (Ну нахрен столько исключений обрабатывать, и нужно ли ввообще писать функцию main или мб хотя бы переменные глобальными делать? Так много вопросов, и так мало ответов...

--- passwords_generator.py
+++ passwords_generator.py
@@ -5,7 +5,6 @@ def main():
     lowercase_letters = 'abcdefghijklmnopqrstuvwxyz'
     uppercase_letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
     punctuation = '!#$%&*+-=?@^_'
-    chars = ''
     try:
         cntPw = int(input('Укажите количество паролей для генерации: '))
         lenPw = int(input('Укажите длину одного пароля: '))
@@ -17,6 +16,27 @@ def main():
     ABC_on = input('Включать ли прописные буквы ABCDEFGHIJKLMNOPQRSTUVWXYZ? (y/n)')
     abc_on = input('Включать ли строчные буквы abcdefghijklmnopqrstuvwxyz? (y/n)')
     ch_on = input('Включать ли символы !#$%&*+-=?@^_? (y/n)')
-    exc_on = input('Исключать ли неоднозначные символы il1Lo0O? (y/n)')
+    chars = generate_chars(((digits, dig_on), (lowercase_letters, abc_on), (uppercase_letters, ABC_on), (punctuation, ch_on)))
+    if chars == None:
+        main()
+        return
+    print(chars)
+    
+def generate_chars(tpl):
+    chars = ''
+    for element, i in tpl:
+        if i == 'y':
+            chars += element
+        elif i != 'n':
+            print('Опять фигню ввел, давай по новой')
+            return
+    exc_on = input('Исключать ли неоднозначные символы il1Lo0O? (y/n or any symbol)')
+    if exc_on == 'y':
+        chars_list = list(chars)
+        for charm in 'il1Lo0O':
+            if charm in chars_list:
+                chars_list.remove(charm)
+        chars = str(chars_list)
+    return chars
     
 main()
