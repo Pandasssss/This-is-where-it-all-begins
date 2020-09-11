45156848af51fb5c658553412a469b61d89660f8
Pandasssss <shkod1998@mail.ru>
2020-09-11 22:21:51 +0300

    v.1.3

--- guessing_game.py
+++ guessing_game.py
@@ -2,14 +2,20 @@ def main():
     import random
     mysterious_num = random.randint(1,100)
     print('Добро пожаловать в числовую угадайку')
-    while True:
+    n = None
+    while n != mysterious_num:
         n = input('Введите число: ')
         if not is_valid(n):
             print('А может быть все-таки введем целое число от 1 до 100?')
             continue
-        if n == mysterious_num:
-            print('Yap')
-            break
+        n = int(n)
+        if n < mysterious_num:
+            print('Ваше число меньше загаданного, попробуйте еще разок')
+        elif n > mysterious_num:
+            print('Ваше число больше загаданного, попробуйте еще разок')
+        elif n == mysterious_num:
+            print('Вы угадали, поздравляем!')
+    print('Спасибо, что играли в числовую угадайку. Еще увидимся...')
     
 def is_valid(n):
     if n.isdigit() and int(n) in range(1,101):
