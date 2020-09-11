3e4121180bca113db6f017e73ce49253f5357600
Pandasssss <shkod1998@mail.ru>
2020-09-11 20:27:18 +0300

    clean

--- guessing_game.py
+++ guessing_game.py
@@ -1,5 +1,13 @@
 def main():
     import random
-    n = random.randint()
+    mysterious_num = random.randint(1,100)
     print('Добро пожаловать в числовую угадайку')
+    n = input('Введите число: ')
+    is_valid(n)
+    
+def is_valid(n):
+    if n.isdigit() and int(n) in range(1,101):
+        return True
+    return False
+
 main()
